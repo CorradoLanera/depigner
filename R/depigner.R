8ff0758cc699fa73664d962387bb718629960069
CorradoL <corrado.lanera@unipd.it>
2017-12-05 17:07:08 +0100

    Initial commit

---
+++ R/depigner.R
@@ -0,0 +1,6 @@
+#' Utilities for _pigna_ stuffs at UBESP
+#'
+#' This package collect usefull function frequently used at the Unit of
+#' Biostatistics, Epidemiology and Public Health of the Department of Cardiac,
+#' Thoracic and Vascular sciences at the university of Padova.
+'depigner'
