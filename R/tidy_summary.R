00b0bf33a644e8cc5d29a1e3f3fc34a0f278805a
CorradoL <corrado.lanera@unipd.it>
2018-11-26 16:28:44 +0100

    tidy_summary method for rms

--- R/tidy_summary.R
+++ R/tidy_summary.R
@@ -6,7 +6,7 @@
 #' \code{\link[knitr]{knit}} the source.
 #'
 #' @param x an object used to select a method, output of some summary
-#'          by \code{\link{Hmisc}}.
+#'          by \code{Hmisc}.
 #' @param ... further arguments passed to or from other methods
 #'
 #' @return a [tibble][tibble::tibble-package]
@@ -18,11 +18,12 @@ tidy_summary <- function(x, ...) {
 
 
 
-#' @inheritParams tidy_summary
-#' @importFrom magrittr %>%
-#' @describeIn tidy_summary tidy a summary reverse output from the
+#' @describeIn tidy_summary Tidies a summary reverse output from the
 #'             \code{\link[Hmisc]{summary.formula}} called with
 #'             \code{method = "reverse"}.
+#'
+#' @inheritParams tidy_summary
+#' @importFrom magrittr %>%
 #'
 #' @note to see the options you can pass to \code{...} for a custom
 #' print, see the print section in \code{\link[Hmisc]{summary.formula}}.
@@ -33,7 +34,7 @@ tidy_summary <- function(x, ...) {
 #' my_summary <- summary(Species ~., data = iris, method = "reverse")
 #' tidy_summary(my_summary)
 tidy_summary.summary.formula.reverse <- function(x, ...) {
-  invisible(capture.output(
+  invisible(utils::capture.output(
     printed <- print(x, ...)
   ))
 
@@ -51,34 +52,68 @@ tidy_summary.summary.formula.reverse <- function(x, ...) {
 
 
 
-#' summary_cox
+#' @describeIn tidy_summary Convert the output of the
+#'             \code{\link[rms]{summary.rms}} into a data frame,
+#'             reporting only the Hazard Ratio with the .95 CI and the
+#'             incremental step (for continuous variables) reference
+#'             (for categorical variables) for which the Hazard is
+#'             refered to (i.e. without \eqn{\beta}s, Low, High, S.E.
+#'             and Type).
 #'
-#' Convert the output of the \code{\link[rms]{summary.rms}} into a data
-#' frame, reporting only the Hazard Ratio with the .95 CI and the incremental
-#' step (for continuous variables) reference (for categorical variables) for
-#' which the Hazard is refered to (i.e. without $\beta$s, Low, High, S.E. and
-#' Type).
+#' @inheritParams tidy_summary
+#' @param diff_digits number of significant digits to use (default 2)
+#'        for the step-difference between continuous variable HR
+#'        computation.
 #'
-#' @param cox_model A `code{cph}` object as produced by \code{\link[rms]{cph}}.
+#' @importFrom magrittr %>%
 #'
-#' @return a [tibble][tibble::tibble-package]
+#' @export
 #'
-#' @note This is a non-exported function, use this at your own risk.
-#' Probably it will change in the near future.
+#' @examples
+#' library(rms)
+#' options(datadist='dd')
 #'
-summary_cox <- function(cox_model) {#, plev = 0.05, hip = TRUE, digits = 3) {
-  broom::tidy(summary(cox_model)) %>%
-  dplyr::mutate(.rownames = lag(.rownames)) %>%
-  dplyr::filter(Type == 2) %>%
-  dplyr::select(-Low, - High, - S.E., -Type) %>%
+#' n <- 1000
+#' set.seed(731)
+#' age <- 50 + 12*rnorm(n)
+#' label(age) <- "Age"
+#' sex <- factor(sample(c('Male','Female'), n,
+#'                      rep=TRUE, prob=c(.6, .4)))
+#' cens <- 15*runif(n)
+#' h <- .02*exp(.04*(age-50)+.8*(sex=='Female'))
+#' dt <- -log(runif(n))/h
+#' label(dt) <- 'Follow-up Time'
+#' e <- ifelse(dt <= cens,1,0)
+#' dt <- pmin(dt, cens)
+#' units(dt) <- "Year"
+#'
+#' dd <- datadist(age, sex)
+#' S <- Surv(dt,e)
+#' f <- cph(S ~ rcs(age,4) + sex, x=TRUE, y=TRUE)
+#'
+#'
+#' my_summary <- summary(f)
+#' tidy_summary(my_summary)
+tidy_summary.summary.rms <- function(x, diff_digits = 2, ...){
+
+  as.data.frame(x) %>%
+    tibble::as_tibble(rownames = ".rownames") %>%
+    dplyr::mutate(.rownames = dplyr::lag(.rownames)) %>%
+    dplyr::filter(Type == 2) %>%
+    .[!names(.) %in% c("Low", "High", "S.E.", "Type")] %>%
   dplyr::mutate(
+    Diff.     = round(Diff., digits = diff_digits),
     Diff.     = ifelse(!is.na(Diff.), Diff.,
-                  stringr::str_extract(.rownames, ' - .*$') %>%
-                  stringr::str_replace(' - ', '')),
-    .rownames = stringr::str_replace(.rownames, ' -+.*$', '')
+                  stringr::str_extract(.rownames, '\\.\\.\\..*$') %>%
+                  stringr::str_replace('\\.\\.\\.', '') %>%
+                  stringr::str_replace('\\.', ':')
+                ),
+    .rownames = stringr::str_replace(.rownames, '\\.\\.\\..*$', '')
   ) %>%
   dplyr::rename(
-    `&nbsp;`     = .rownames,
-    `Hazard Ratio` = Effect
+    `&nbsp;`       = .rownames,
+    `HR` = Effect,
+    `Lower 95% CI`   = `Lower 0.95`,
+    `Upper 95% CI`   = `Upper 0.95`
   )
 }
