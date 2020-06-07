199d1a847ae738e3aa4e26c0cc4325f3327eac33
Corrado <corrado.lanera@gmail.com>
2020-06-07 21:18:59 +0200

    Remove explicit call to testthat:: in `Htypes()`' tests

--- tests/testthat/test-Htypes.R
+++ tests/testthat/test-Htypes.R
@@ -1,61 +1,61 @@
-testthat::test_that("single check works", {
+test_that("single check works", {
   test_desc <- Hmisc::describe(mtcars)
-  testthat::expect_false(is_single_Hdesc(test_desc))
-  testthat::expect_true(is_single_Hdesc(test_desc[[1]]))
-  testthat::expect_false(is_single_Hdesc(mtcars))
+  expect_false(is_single_Hdesc(test_desc))
+  expect_true(is_single_Hdesc(test_desc[[1]]))
+  expect_false(is_single_Hdesc(mtcars))
 })
 
 
-testthat::test_that("original check works", {
+test_that("original check works", {
   test_desc <- Hmisc::describe(mtcars)
-  testthat::expect_true(is_Hdesc(test_desc))
-  testthat::expect_true(is_Hdesc(test_desc[[1]]))
-  testthat::expect_false(is_Hdesc(mtcars))
+  expect_true(is_Hdesc(test_desc))
+  expect_true(is_Hdesc(test_desc[[1]]))
+  expect_false(is_Hdesc(mtcars))
 })
 
 
-testthat::test_that("cat check works", {
+test_that("cat check works", {
   test_desc <- Hmisc::describe(mtcars)
-  testthat::expect_true(is_Hcat(test_desc[["vs"]]))
-  testthat::expect_false(is_Hcat(test_desc[["mpg"]]))
+  expect_true(is_Hcat(test_desc[["vs"]]))
+  expect_false(is_Hcat(test_desc[["mpg"]]))
 
   skip_if(as.integer(R.Version()$major) < 4)
-  testthat::expect_error(is_Hcat(test_desc), "must be a single")
+  expect_error(is_Hcat(test_desc), "must be a single")
 })
 
 
-testthat::test_that("original check works", {
+test_that("original check works", {
   test_desc <- Hmisc::describe(mtcars)
-  testthat::expect_false(is_Hcon(test_desc[["vs"]]))
-  testthat::expect_true(is_Hcon(test_desc[["mpg"]]))
+  expect_false(is_Hcon(test_desc[["vs"]]))
+  expect_true(is_Hcon(test_desc[["mpg"]]))
 
   test_nunique <- Hmisc::describe(airquality)
-  testthat::expect_false(is_Hcon(test_nunique[["Month"]]))
-  testthat::expect_true(
+  expect_false(is_Hcon(test_nunique[["Month"]]))
+  expect_true(
     is_Hcon(test_nunique[["Month"]], n.unique = 4)
   )
 
   skip_if(as.integer(R.Version()$major) < 4)
-  testthat::expect_error(is_Hcon(test_desc), "must be a single")
+  expect_error(is_Hcon(test_desc), "must be a single")
 })
 
 
-testthat::test_that("Htype works", {
+test_that("Htype works", {
   test_desc <- Hmisc::describe(mtcars)
-  testthat::expect_equal(Htype(test_desc[["vs"]]), "cat")
-  testthat::expect_equal(Htype(test_desc[["mpg"]]), "con")
-  testthat::expect_equal(Htype(test_desc[["carb"]]), "none")
-  testthat::expect_equal(
+  expect_equal(Htype(test_desc[["vs"]]), "cat")
+  expect_equal(Htype(test_desc[["mpg"]]), "con")
+  expect_equal(Htype(test_desc[["carb"]]), "none")
+  expect_equal(
     Htype(test_desc[["carb"]], n.unique = 4),
     "con"
   )
 
   skip_if(as.integer(R.Version()$major) < 4)
-  testthat::expect_error(Htype(test_desc), "must be a single")
+  expect_error(Htype(test_desc), "must be a single")
 })
 
 
-testthat::test_that("Htypes works", {
+test_that("Htypes works", {
   test_desc <- Hmisc::describe(mtcars)
 
   # Note: the following expectation was deduced from the output of
@@ -70,23 +70,23 @@ testthat::test_that("Htypes works", {
   ## all together
   # Note: `expect_setequal()` because the order would be probabily
   #       different.
-  testthat::expect_equal(
+  expect_equal(
     Htypes(test_desc)[order(names(Htypes(test_desc)))],
     expected[order(names(expected))]
   )
 
   ## one at time
-  testthat::expect_equal(Htypes(test_desc[["mpg"]]), "con")
-  testthat::expect_equal(Htypes(test_desc[["vs"]]), "cat")
-  testthat::expect_equal(Htypes(test_desc[["cyl"]]), "none")
+  expect_equal(Htypes(test_desc[["mpg"]]), "con")
+  expect_equal(Htypes(test_desc[["vs"]]), "cat")
+  expect_equal(Htypes(test_desc[["cyl"]]), "none")
 
   ## input directly a data.frame
-  testthat::expect_equal(
+  expect_equal(
     Htypes(mtcars)[order(names(Htypes(test_desc)))],
     expected[order(names(expected))]
   )
 
   ## input a single vector
   # Note: `plot(describe(letters))` throws an error
-  testthat::expect_equal(Htypes(letters), "none")
+  expect_equal(Htypes(letters), "none")
 })
