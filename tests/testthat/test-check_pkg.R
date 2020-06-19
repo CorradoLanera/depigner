3a14bd37bb25e159db6a7e7f3fa82d7b284d6718
Corrado <corrado.lanera@gmail.com>
2020-06-19 19:46:20 +0200

    update test for interactive runs

--- tests/testthat/test-check_pkg.R
+++ tests/testthat/test-check_pkg.R
@@ -1,8 +1,12 @@
 context("test-install_pkg_set")
 
 test_that("empty please_install()", {
+
   expect_null(please_install(character(0)))
-  expect_error(please_install("stats"),
-    "Please run in interactive session", class = "usethis_error"
-  )
+
+  if (!interactive()) {
+    expect_error(please_install("stats"),
+      "Please run in interactive session", class = "usethis_error"
+    )
+  }
 })
