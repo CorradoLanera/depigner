f846abaa00d4c58277fc8865076ae660c631eb09
Corrado <corrado.lanera@gmail.com>
2020-06-19 20:30:38 +0200

    more clear comments on dev-checks

--- dev/dev-checks.R
+++ dev/dev-checks.R
@@ -5,13 +5,14 @@ spelling::update_wordlist()
 
 ## Before pull requests
 devtools::test()
-# devtools::test() # from RStudio button, ie non interactive session!
+# devtools::test() # from RStudio button too: non interactive session!
 covr::report()
 
 lintr::lint_package()
 goodpractice::gp()
 
 devtools::check()
+# devtools::check() # from RStudio button too: non interactive session!
 
 
 ## Befor CRAN
