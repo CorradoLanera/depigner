809a8e054f2ddfee01a141752de2a816131cb96f
CorradoLanera <corrado.lanera@gmail.com>
2020-10-07 18:50:30 +0200

    update tests for telegram.bot (fix #33)

--- tests/testthat/test-chat_id_from_name.R
+++ tests/testthat/test-chat_id_from_name.R
@@ -4,7 +4,7 @@ test_that("it stops if bot is not setup", {
 
 test_that("it stops on inexistent chats name", {
   skip_if(!getOption("depigner.dev.test_telegram_bot"))
-  skip_if(Sys.getenv("R_telegram_bot_name") != "clr_telegram_bot")
+  skip_if(Sys.getenv("R_telegram_bot_name") != "cl_r_bot")
 
   start_bot_for_chat("Depigner test")
 
@@ -17,11 +17,11 @@ test_that("it stops on inexistent chats name", {
 
 test_that("correct know id", {
   skip_if(!getOption("depigner.dev.test_telegram_bot"))
-  skip_if(Sys.getenv("R_telegram_bot_name") != "clr_telegram_bot")
+  skip_if(Sys.getenv("R_telegram_bot_name") != "cl_r_bot")
 
   start_bot_for_chat("Depigner test")
-  expect_equal(chat_id_from_name("Depigner test"), -297554410)
+  expect_equal(chat_id_from_name("Depigner test"), -414181402)
 
-  options(depigner.bot = "")
-  options(depigner.chat_id = "")
+  options(depigner.bot = NULL)
+  options(depigner.chat_id = NULL)
 })
